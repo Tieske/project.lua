e258f5baa8a9bdf522f54392aae4aee9e02c6311
Thijs Schreijer <thijs@thijsschreijer.nl>
2022-09-12 10:31:31 +0200

    chore(test) make initial test slightly more useful

--- spec/01-first-test_spec.lua
+++ spec/01-first-test_spec.lua
@@ -1,9 +1,17 @@
-describe("description", function()
+describe("description: replace this file with your own tests", function()
 
-  local mymod = require "[module-name]"
 
-  it("replace this file with your own tests", function()
-    mymod.do_something("tell a lie!", true)
+  it("loads the module", function()
+    local mymod
+
+    assert.has.no.error(function()
+      mymod = require "[module-name]"
+    end)
+
+    assert.is.table(mymod)
+    assert.is.string(mymod._VERSION)
+    assert.is.string(mymod._COPYRIGHT)
+    assert.is.string(mymod._DESCRIPTION)
   end)
 
 end)
